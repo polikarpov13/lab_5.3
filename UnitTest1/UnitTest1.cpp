8cc4f3ef5cc372050ba09c7348dbefd82c7069eb
{Vlad} <{vladpolikarpovvl@gmail.com}>
2022-05-30 14:01:23 +0300

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../../../../політех/ооп/5/lab_5.3/lab_5.3/lab_5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			A a;
+			a.setX(10);
+
+			Assert::AreEqual(10, a.getX());
+		}
+	};
+}
---
+++ lab_5.3/lab_5.3.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class A
+{
+protected:
+	int x;
+public:
+	A(int x = 0)
+	{
+		this->x = x;
+	}
+
+	void setX(int X) {
+		x = X;
+	}
+
+	int getX() {
+		return x;
+	}
+};
+class B
+	: public A
+{
+	int y;
+public:
+	B(int x = 0, int y = 0)
+		: A(x)
+	{
+		this->y = y;
+	}
+	B(A a)
+	{
+		throw a;
+	}
+};
+int main() {
+	A a(1);
+	B b(2, 3);
+	try
+	{
+		b = B(a);
+		cout << "Continue" << endl;
+	}
+	catch (B)
+	{
+		cout << "catch(B)" << endl;
+		exit(1);
+	}
+	catch (A)
+	{
+		cout << "catch(A)" << endl;
+		exit(2);
+	}
+
+	cout << "End" << endl;
+
+	return 0;
+}
